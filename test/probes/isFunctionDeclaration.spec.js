7e6f2597e54f3014d2c147eca9491fe2bb9d9575
PierreD <39910767+PierreDemailly@users.noreply.github.com>
2022-09-02 18:45:10 +0200

    test: add isFunctionDeclaration tests (#47)
    * test: add isFunctionDeclaration tests
    
    * refactor: remove template literals

---
+++ test/probes/isFunctionDeclaration.spec.js
@@ -0,0 +1,26 @@
+// Require Internal Dependencies
+import { getSastAnalysis, parseScript } from "../utils/index.js";
+import isFunctionDeclaration from "../../src/probes/isFunctionDeclaration.js";
+
+// Require Third-party dependencies
+import test from "tape";
+
+test("should detect 1 function declaration", (tape) => {
+  const str = "function foo() {}";
+  const ast = parseScript(str);
+  const analysis = getSastAnalysis(str, ast.body, isFunctionDeclaration);
+
+  tape.equal(analysis.idtypes.functionDeclaration, 1);
+
+  tape.end();
+});
+
+test("should detect 0 function declaration", (tape) => {
+  const str = "foo()";
+  const ast = parseScript(str);
+  const analysis = getSastAnalysis(str, ast.body, isFunctionDeclaration);
+
+  tape.equal(analysis.idtypes.functionDeclaration, 0);
+
+  tape.end();
+});
