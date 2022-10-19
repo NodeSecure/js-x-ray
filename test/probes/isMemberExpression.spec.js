7e15afd3f6df9237713553d347c482f301b4dbfd
Maji <steeven.regnault@outlook.fr>
2022-10-19 21:26:18 +0200

    test: add ut for computed expression in isMemberExpression

--- test/probes/isMemberExpression.spec.js
+++ test/probes/isMemberExpression.spec.js
@@ -25,6 +25,17 @@ test("should detect 2 members expressions", (tape) => {
   tape.end();
 });
 
+test("should detect 1 member expression and 2 nodes", (tape) => {
+  const str = "process.mainModule['foo']['bar']";
+  const ast = parseScript(str);
+  const analysis = getSastAnalysis(str, ast.body, isMemberExpression);
+
+  tape.equal(analysis.counter.memberExpr, 1);
+  tape.equal(analysis.counter.computedMemberExpr, 2);
+
+  tape.end();
+});
+
 test("should detect 0 member expression", (tape) => {
   const str = "process";
   const ast = parseScript(str);
