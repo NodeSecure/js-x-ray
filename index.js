eae16ebc2d5c175024546beb2d2b01f503ab65d4
jean <jean.antoine.michelet@gmail.com>
2024-01-17 11:20:58 +0100

    remove obsolet check

--- index.js
+++ index.js
@@ -47,7 +47,7 @@ export function runASTAnalysis(
   return {
     ...source.getResult(isMinified),
     dependencies: source.dependencies,
-    isOneLineRequire: body.length <= 1 && (source.dependencies.size <= 1 || isOneLineExpressionExport(body))
+    isOneLineRequire: source.dependencies.size <= 1 || isOneLineExpressionExport(body)
   };
 }
 
