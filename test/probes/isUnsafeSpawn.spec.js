886827a973f4ef6dd132d1c7c36b69d6202ded49
Tony Gorez <gorez.tony@gmail.com>
2025-06-10 09:02:20 +0200

    test: add last (missing) case
    Signed-off-by: Tony Gorez <gorez.tony@gmail.com>

--- test/probes/isUnsafeSpawn.spec.js
+++ test/probes/isUnsafeSpawn.spec.js
@@ -24,6 +24,21 @@ test("should detect csrutil spawn command", () => {
   assert.equal(result.value, "csrutil");
 });
 
+test("should detect hidden csrutil spawn command", () => {
+  const str = `
+    const { spawn: hide } = require("child_process");
+    hide("csrutil", ["status"]);
+  `;
+
+  const ast = parseScript(str);
+  const sastAnalysis = getSastAnalysis(str, isUnsafeSpawn)
+    .execute(ast.body);
+
+  const result = sastAnalysis.getWarning(kWarningUnsafeSpawn);
+  assert.equal(result.kind, kWarningUnsafeSpawn);
+  assert.equal(result.value, "csrutil");
+});
+
 test("should detect csrutil spawn command with require", () => {
   const str = `
     require("child_process").spawn("csrutil", ["disable"]);
