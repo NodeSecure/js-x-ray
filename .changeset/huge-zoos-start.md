a98fe96418e7da1ca90a735f6811bf78ae486f51
Thomas.G <gentilhomme.thomas@gmail.com>
2025-11-07 10:34:10 +0100

    refactor: implement isStringBase64 and remove unmaintained dep (#414)

---
+++ .changeset/huge-zoos-start.md
@@ -0,0 +1,5 @@
+---
+"@nodesecure/sec-literal": minor
+---
+
+Remove un-maintained is-base64 package and re-implement the function in the sec-literal project with proper TS definition
--- workspaces/sec-literal/src/utils.ts
+++ workspaces/sec-literal/src/utils.ts
@@ -5,6 +5,38 @@ import stringWidth from "string-width";
 // Import Internal Dependencies
 import { toValue, type ESTreeLiteral } from "./literal.js";
 
+export interface IsBase64Options {
+  allowMime?: boolean;
+  mimeRequired?: boolean;
+  paddingRequired?: boolean;
+  allowEmpty?: boolean;
+}
+
+export function isStringBase64(
+  v: string,
+  opts: IsBase64Options = {}
+): boolean {
+  if (opts.allowEmpty === false && v === "") {
+    return false;
+  }
+
+  let regex = "(?:[A-Za-z0-9+\\/]{4})*(?:[A-Za-z0-9+/]{2}==|[A-Za-z0-9+\\/]{3}=)?";
+  const mimeRegex = "(data:\\w+\\/[a-zA-Z\\+\\-\\.]+;base64,)";
+
+  if (opts.mimeRequired === true) {
+    regex = mimeRegex + regex;
+  }
+  else if (opts.allowMime === true) {
+    regex = mimeRegex + "?" + regex;
+  }
+
+  if (opts.paddingRequired === false) {
+    regex = "(?:[A-Za-z0-9+\\/]{4})*(?:[A-Za-z0-9+\\/]{2}(==)?|[A-Za-z0-9+\\/]{3}=?)?";
+  }
+
+  return (new RegExp("^" + regex + "$", "gi")).test(v);
+}
+
 export function isSvg(
   strOrLiteral: ESTreeLiteral | string
 ): boolean {
