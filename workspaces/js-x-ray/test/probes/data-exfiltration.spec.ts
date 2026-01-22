e288c045fe24f58b4344350a23d9b2d2b7f1b824
Clement Gombauld <clementgombauld@hotmail.fr>
2026-01-22 16:52:12 +0100

    feat: generate data-exfiltration warning on import when the sensitivity is aggressive (#471)

--- workspaces/js-x-ray/test/probes/data-exfiltration.spec.ts
+++ workspaces/js-x-ray/test/probes/data-exfiltration.spec.ts
@@ -10,84 +10,85 @@ import { AstAnalyser } from "../../src/index.ts";
 const FIXTURE_URL = new URL("fixtures/dataExfiltration/", import.meta.url);
 
 describe("data exfiltration", () => {
-  test("it should report a warning in case of `JSON.stringify(sensitiveData) for member expression`", async() => {
-    const fixturesDir = new URL("memberExpression/", FIXTURE_URL);
-    const fixtureFiles = await fs.readdir(fixturesDir);
+  describe("sensitivity: conservative", () => {
+    test("it should report a warning in case of `JSON.stringify(sensitiveData) for member expression`", async () => {
+      const fixturesDir = new URL("memberExpression/", FIXTURE_URL);
+      const fixtureFiles = await fs.readdir(fixturesDir);
 
-    for (const fixtureFile of fixtureFiles) {
-      const fixture = readFileSync(new URL(fixtureFile, fixturesDir), "utf-8");
-      const { warnings: outputWarnings } = new AstAnalyser(
-        {
-          optionalWarnings: true
-        }
-      ).analyse(fixture);
+      for (const fixtureFile of fixtureFiles) {
+        const fixture = readFileSync(new URL(fixtureFile, fixturesDir), "utf-8");
+        const { warnings: outputWarnings } = new AstAnalyser(
+          {
+            optionalWarnings: true
+          }
+        ).analyse(fixture);
 
-      const [firstWarning] = outputWarnings;
-      assert.strictEqual(outputWarnings.length, 1);
-      assert.deepEqual(firstWarning.kind, "data-exfiltration");
-      assert.strictEqual(firstWarning.value, `${fixtureFile.split(".").slice(0, 2).join(".")}`);
-    }
-  });
+        const [firstWarning] = outputWarnings;
+        assert.strictEqual(outputWarnings.length, 1);
+        assert.deepEqual(firstWarning.kind, "data-exfiltration");
+        assert.strictEqual(firstWarning.value, `${fixtureFile.split(".").slice(0, 2).join(".")}`);
+      }
+    });
 
-  test("it should report a warning in case of `JSON.stringify(sensitiveData) for direct call expression`", async() => {
-    const fixturesDir = new URL("directCallExpression/", FIXTURE_URL);
-    const fixtureFiles = await fs.readdir(fixturesDir);
+    test("it should report a warning in case of `JSON.stringify(sensitiveData) for direct call expression`", async () => {
+      const fixturesDir = new URL("directCallExpression/", FIXTURE_URL);
+      const fixtureFiles = await fs.readdir(fixturesDir);
 
-    for (const fixtureFile of fixtureFiles) {
-      const fixture = readFileSync(new URL(fixtureFile, fixturesDir), "utf-8");
-      const { warnings: outputWarnings } = new AstAnalyser(
-        {
-          optionalWarnings: true
-        }
-      ).analyse(fixture);
+      for (const fixtureFile of fixtureFiles) {
+        const fixture = readFileSync(new URL(fixtureFile, fixturesDir), "utf-8");
+        const { warnings: outputWarnings } = new AstAnalyser(
+          {
+            optionalWarnings: true
+          }
+        ).analyse(fixture);
 
-      const [firstWarning] = outputWarnings;
-      assert.strictEqual(outputWarnings.length, 1);
-      assert.deepEqual(firstWarning.kind, "data-exfiltration");
-      assert.strictEqual(firstWarning.value, `${fixtureFile.split(".").slice(0, 2).join(".")}`);
-    }
-  });
+        const [firstWarning] = outputWarnings;
+        assert.strictEqual(outputWarnings.length, 1);
+        assert.deepEqual(firstWarning.kind, "data-exfiltration");
+        assert.strictEqual(firstWarning.value, `${fixtureFile.split(".").slice(0, 2).join(".")}`);
+      }
+    });
 
-  test("should only generate one warning when multiple detection of data exfiltration occurs", () => {
-    const code = `
-     import os from "os"; 
+    test("should only generate one warning when multiple detection of data exfiltration occurs", () => {
+      const code = `
+     import os from "os";
 
-     JSON.stringify(os.userInfo()); 
-     JSON.stringify(os.userInfo()); 
-     JSON.stringify(os.networkInterfaces()); 
+     JSON.stringify(os.userInfo());
+     JSON.stringify(os.userInfo());
+     JSON.stringify(os.networkInterfaces());
     `;
 
-    const { warnings: outputWarnings } = new AstAnalyser(
-      {
-        optionalWarnings: true
-      }
-    ).analyse(code);
+      const { warnings: outputWarnings } = new AstAnalyser(
+        {
+          optionalWarnings: true
+        }
+      ).analyse(code);
 
-    const [firstWarning] = outputWarnings;
-    assert.strictEqual(outputWarnings.length, 1);
-    assert.deepEqual(firstWarning.kind, "data-exfiltration");
-    assert.strictEqual(firstWarning.value, "os.userInfo, os.networkInterfaces");
-    assert.strictEqual(firstWarning.location?.length, 3);
-  });
+      const [firstWarning] = outputWarnings;
+      assert.strictEqual(outputWarnings.length, 1);
+      assert.deepEqual(firstWarning.kind, "data-exfiltration");
+      assert.strictEqual(firstWarning.value, "os.userInfo, os.networkInterfaces");
+      assert.strictEqual(firstWarning.location?.length, 3);
+    });
 
-  test("should not generate a warning when serializing return value of every function call", () => {
-    const code = `
+    test("should not generate a warning when serializing return value of every function call", () => {
+      const code = `
      function foo (){
         return "foo";
      }
-     JSON.stringify(foo()); 
+     JSON.stringify(foo());
     `;
-    const { warnings: outputWarnings } = new AstAnalyser(
-      {
-        optionalWarnings: true
-      }
-    ).analyse(code);
+      const { warnings: outputWarnings } = new AstAnalyser(
+        {
+          optionalWarnings: true
+        }
+      ).analyse(code);
 
-    assert.strictEqual(outputWarnings.length, 0);
-  });
+      assert.strictEqual(outputWarnings.length, 0);
+    });
 
-  test("should not generate a warning when os is not imported", () => {
-    const code = `
+    test("should not generate a warning when os is not imported", () => {
+      const code = `
      const os = {
         userInfo(){
             return {};
@@ -98,36 +99,127 @@ describe("data exfiltration", () => {
         networkInterfaces(){
             return [];
         }
-        
+
      }
-     JSON.stringify(os.userInfo()); 
-     JSON.stringify(os.networkInterfaces()); 
-     JSON.stringify(os.cpus()); 
+     JSON.stringify(os.userInfo());
+     JSON.stringify(os.networkInterfaces());
+     JSON.stringify(os.cpus());
     `;
-    const { warnings: outputWarnings } = new AstAnalyser(
-      {
-        optionalWarnings: true
-      }
-    ).analyse(code);
+      const { warnings: outputWarnings } = new AstAnalyser(
+        {
+          optionalWarnings: true
+        }
+      ).analyse(code);
 
-    assert.strictEqual(outputWarnings.length, 0);
-  });
+      assert.strictEqual(outputWarnings.length, 0);
+    });
 
-  test("should not generate a warning when dns is not imported", () => {
-    const code = `
+    test("should not generate a warning when dns is not imported", () => {
+      const code = `
      const dns = {
         getServers(){
             return [];
         }
      }
-     JSON.stringify(getServers()); 
+     JSON.stringify(getServers());
     `;
-    const { warnings: outputWarnings } = new AstAnalyser(
-      {
-        optionalWarnings: true
-      }
-    ).analyse(code);
+      const { warnings: outputWarnings } = new AstAnalyser(
+        {
+          optionalWarnings: true
+        }
+      ).analyse(code);
 
-    assert.strictEqual(outputWarnings.length, 0);
+      assert.strictEqual(outputWarnings.length, 0);
+    });
+  });
+
+  describe("sensitivity: aggressive", () => {
+    test("should generate a warning as soon as a os import is detected", () => {
+      const code = `
+     import os from "node:os";
+    `;
+      const { warnings: outputWarnings } = new AstAnalyser(
+        {
+          optionalWarnings: true,
+          sensitivity: "aggressive"
+        }
+      ).analyse(code);
+
+      const [firstWarning] = outputWarnings;
+      assert.strictEqual(outputWarnings.length, 1);
+      assert.deepEqual(firstWarning.kind, "data-exfiltration");
+      assert.strictEqual(firstWarning.value, "os");
+    });
+
+    test("should generate a warning as soon as a dns import is detected", () => {
+      const code = `
+     import dns from "node:dns";
+    `;
+      const { warnings: outputWarnings } = new AstAnalyser(
+        {
+          optionalWarnings: true,
+          sensitivity: "aggressive"
+        }
+      ).analyse(code);
+
+      const [firstWarning] = outputWarnings;
+      assert.strictEqual(outputWarnings.length, 1);
+      assert.deepEqual(firstWarning.kind, "data-exfiltration");
+      assert.strictEqual(firstWarning.value, "dns");
+    });
+
+    test("should not generate a warning when the import is not from the expected modules module", () => {
+      const code = `
+     import * as c from "crypto";
+    `;
+      const { warnings: outputWarnings } = new AstAnalyser(
+        {
+          optionalWarnings: true,
+          sensitivity: "aggressive"
+        }
+      ).analyse(code);
+
+      assert.strictEqual(outputWarnings.length, 0);
+    });
+
+    test("should not detect the conservative warning", () => {
+      const code = `
+     import dns from "node:dns";
+
+
+     JSON.stringify(dns.getServers());
+    `;
+      const { warnings: outputWarnings } = new AstAnalyser(
+        {
+          optionalWarnings: true,
+          sensitivity: "aggressive"
+        }
+      ).analyse(code);
+
+      const [firstWarning] = outputWarnings;
+      assert.strictEqual(outputWarnings.length, 1);
+      assert.deepEqual(firstWarning.kind, "data-exfiltration");
+      assert.strictEqual(firstWarning.value, "dns");
+    });
+
+    test("should generate only one warning when multiple detection occurs", () => {
+      const code = `
+     import dns from "node:dns";
+     import os from "node:os";
+    `;
+
+      const { warnings: outputWarnings } = new AstAnalyser(
+        {
+          optionalWarnings: true,
+          sensitivity: "aggressive"
+        }
+      ).analyse(code);
+
+      const [firstWarning] = outputWarnings;
+      assert.strictEqual(outputWarnings.length, 1);
+      assert.deepEqual(firstWarning.kind, "data-exfiltration");
+      assert.strictEqual(firstWarning.value, "dns, os");
+      assert.strictEqual(firstWarning.location?.length, 2);
+    });
   });
 });
