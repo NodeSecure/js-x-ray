67d376fbedcf55f0b276bca7ba0c8ce7f7cc4cb8
bashlor <39790502+bashlor@users.noreply.github.com>
2026-02-22 12:28:32 +0100

    refactor(test): destructure body from parseScript

--- workspaces/js-x-ray/test/utils/isOneLineExpressionExport.spec.ts
+++ workspaces/js-x-ray/test/utils/isOneLineExpressionExport.spec.ts
@@ -14,51 +14,51 @@ describe("isOneLineExpressionExport()", () => {
   });
 
   it("should return false for multiple statements", () => {
-    const body = parseScript(`
+    const { body } = parseScript(`
         require('a');
         require('b');
-    `).body;
+    `);
     assert.strictEqual(isOneLineExpressionExport(body), false);
   });
 
   it("should return true for single require call", () => {
-    const body = parseScript("require('a');").body;
+    const { body } = parseScript("require('a');");
     assert.strictEqual(isOneLineExpressionExport(body), true);
   });
 
   it("should return true for module.exports assignment to require", () => {
-    const body = parseScript("module.exports = require('a');").body;
+    const { body } = parseScript("module.exports = require('a');");
     assert.strictEqual(isOneLineExpressionExport(body), true);
   });
 
   it("should return true for conditional require export", () => {
-    const body = parseScript(`
+    const { body } = parseScript(`
         module.exports = condition ? require('a') : require('b');
-        `).body;
+        `);
     assert.strictEqual(isOneLineExpressionExport(body), true);
   });
 
   it("should return true for logical require export", () => {
-    const body = parseScript(`
+    const { body } = parseScript(`
         module.exports = condition && require('b');
-        `).body;
+        `);
     assert.strictEqual(isOneLineExpressionExport(body), true);
   });
 
   it("should return false for non-require expression", () => {
-    const body = parseScript("module.exports = foo();").body;
+    const { body } = parseScript("module.exports = foo();");
     assert.strictEqual(isOneLineExpressionExport(body), false);
   });
 
   it("should return true for require member access", () => {
-    const body = parseScript(`
+    const { body } = parseScript(`
     module.exports = require("foo").bar.baz;
-  `).body;
+  `);
     assert.strictEqual(isOneLineExpressionExport(body), true);
   });
 
   it("should return false for non-require member access", () => {
-    const body = parseScript(`module.exports = something.require("foo");`).body;
+    const { body } = parseScript(`module.exports = something.require("foo");`);
     assert.strictEqual(isOneLineExpressionExport(body), false);
   });
 });
